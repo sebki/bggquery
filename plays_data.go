799323f020ac4b31abaeb30e134967f4e94c5b86
sebki <sebastian@kiedaisch.net>
2020-08-06 12:11:42 +0200

    plays_data.go added

---
+++ plays_data.go
@@ -0,0 +1,60 @@
+package bggquery
+
+import "encoding/xml"
+
+// PlaysItem contains response data from a Plays-request to boardgamegeek
+type PlaysItems struct {
+	XMLName    xml.Name `xml:"plays"`
+	Text       string   `xml:",chardata"`
+	Total      string   `xml:"total,attr"`
+	Page       string   `xml:"page,attr"`
+	Termsofuse string   `xml:"termsofuse,attr"`
+	Play       []struct {
+		Text       string `xml:",chardata"`
+		ID         string `xml:"id,attr"`
+		Userid     string `xml:"userid,attr"`
+		Date       string `xml:"date,attr"`
+		Quantity   string `xml:"quantity,attr"`
+		Length     string `xml:"length,attr"`
+		Incomplete string `xml:"incomplete,attr"`
+		Nowinstats string `xml:"nowinstats,attr"`
+		Location   string `xml:"location,attr"`
+		Item       struct {
+			Text       string `xml:",chardata"`
+			Name       string `xml:"name,attr"`
+			Objecttype string `xml:"objecttype,attr"`
+			Objectid   string `xml:"objectid,attr"`
+			Subtypes   struct {
+				Text    string `xml:",chardata"`
+				Subtype []struct {
+					Text  string `xml:",chardata"`
+					Value string `xml:"value,attr"`
+				} `xml:"subtype"`
+			} `xml:"subtypes"`
+		} `xml:"item"`
+		Comments string `xml:"comments"`
+		Players  struct {
+			Text   string `xml:",chardata"`
+			Player []struct {
+				Text          string `xml:",chardata"`
+				Username      string `xml:"username,attr"`
+				Userid        string `xml:"userid,attr"`
+				Name          string `xml:"name,attr"`
+				Startposition string `xml:"startposition,attr"`
+				Color         string `xml:"color,attr"`
+				Score         string `xml:"score,attr"`
+				New           string `xml:"new,attr"`
+				Rating        string `xml:"rating,attr"`
+				Win           string `xml:"win,attr"`
+			} `xml:"player"`
+		} `xml:"players"`
+	} `xml:"play"`
+}
+
+func (pi *PlaysItems) Write(b []byte) (n int, err error) {
+	err = xml.Unmarshal(b, pi)
+	if err != nil {
+		return 0, err
+	}
+	return len(b), nil
+}
