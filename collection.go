e48603d5d4251773f14df7fdbf4c316494d5087a
sebki <sebastian@kiedaisch.net>
2020-08-11 15:22:16 +0200

    added first methods for CollectionQuery

--- collection.go
+++ collection.go
@@ -6,25 +6,25 @@ import "time"
 type CollectionQuery struct {
 	username         string
 	version          bool
-	subtType         ThingType
+	subType          ThingType
 	excludeSubType   ThingType
 	ids              []string
 	brief            bool
 	stats            bool
-	own              bool
-	rated            bool
-	played           bool
-	comment          bool
-	trade            bool
-	want             bool
-	wishlist         bool
+	own              int
+	rated            int
+	played           int
+	comment          int
+	trade            int
+	want             int
+	wishlist         int
 	wishlistPriority int
-	preOrdered       bool
-	wantToPlay       bool
-	wantToBuy        bool
-	prevOwned        bool
-	hasParts         bool
-	wantParts        bool
+	preOrdered       int
+	wantToPlay       int
+	wantToBuy        int
+	prevOwned        int
+	hasParts         int
+	wantParts        int
 	minRating        int
 	maxRating        int
 	minBggRating     int
@@ -34,4 +34,32 @@ type CollectionQuery struct {
 	showPrivate      bool
 	collID           string
 	modifiedSince    time.Time
+}
+
+// NewCollectionQuery returns a new CollectionQuery
+func NewCollectionQuery(username string) *CollectionQuery {
+	cq := CollectionQuery{
+		username: username,
+	}
+	return &cq
+}
+
+// EnableVersion returns version info for each item in collection
+func (cq *CollectionQuery) EnableVersion() {
+	cq.version = true
+}
+
+// SetSubtype sets type of collection, if empty type is boardgame
+func (cq *CollectionQuery) SetSubtype(subType ThingType) {
+	cq.subType = subType
+}
+
+// ExcludeSubtype specifies which type should be excluded
+func (cq *CollectionQuery) ExcludeSubtype(subType ThingType) {
+	cq.excludeSubType = subType
+}
+
+// FilerIDs filters collection to specified items
+func (cq *CollectionQuery) FilterIDs(ids []string) {
+	cq.ids = ids
 }
